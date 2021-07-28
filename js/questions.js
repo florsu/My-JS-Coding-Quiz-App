eda35eb22d2fae8113d38b4e8e8c24892a6597e9
Christian Flores <christiangflores0824@gmail.com>
2021-07-28 00:17:06 -0700

    create an array of questions

---
+++ js/questions.js
@@ -0,0 +1,19 @@
+const quiz = [
+   {
+    quiz: 'What keyword can you use to define a variable?',
+   },
+   {
+    quiz: 'What will printIn(2+2) print to the screen?',
+   },
+   {
+    quiz: 'What symbol do you use to do division in JavaScript?',
+   },
+   {
+    quiz: 'What can we use inside of a loop to end it?',
+   },
+   {
+    quiz: 'What do you need to call to ask the user of the program to enter text?',
+   },
+
+]
+console.log(quiz)
