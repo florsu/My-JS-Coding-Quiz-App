99ec536e068cd2ba8e0e9fd9b9f7a4c07b28ca8c
Christian Flores <christiangflores0824@gmail.com>
2021-07-30 01:01:06 -0700

    add choices

--- js/questions.js
+++ js/questions.js
@@ -1,19 +1,52 @@
 const quiz = [
    {
-    quiz: 'What keyword can you use to define a variable?',
+      quiz: 'What keyword can you use to define a variable?',
+      choices: [
+         { choice: 'let', correct: false },
+         { choice: 'const', correct: false },
+         { choice: 'var', correct: false },
+         { choice: 'All of the above', correct: true },
+      ]
    },
    {
-    quiz: 'What will printIn(2+2) print to the screen?',
+      quiz: 'What will printIn(2+2) print to the screen?',
+      choices: [
+         { choice: '2+2', correct: false },
+         { choice: '4', correct: true },
+         { choice: 'NaN', correct: false },
+         { choice: 'undefined', correct: false },
+      ]
    },
    {
-    quiz: 'What symbol do you use to do division in JavaScript?',
+      quiz: 'What symbol do you use to do division in JavaScript?',
+      choices: [
+         { choice: '÷', correct: false },
+         { choice: '*', correct: false },
+         { choice: '%', correct: false },
+         { choice: '/', correct: true },
+      ]
    },
    {
-    quiz: 'What can we use inside of a loop to end it?',
+      quiz: 'What can we use inside of a loop to end it?',
+      choices: [
+         { choice: 'return', correct: false },
+         { choice: 'break', correct: true },
+         { choice: 'false', correct: false },
+         { choice: 'true', correct: false },
+      ]
    },
    {
-    quiz: 'What do you need to call to ask the user of the program to enter text?',
+      quiz: 'What do you need to call to ask the user of the program to enter text?',
+      choices: [
+         { choice: 'printIn', correct: false },
+         { choice: 'sentinel', correct: false },
+         { choice: 'readLine', correct: true },
+         { choice: 'getText', correct: false },
+      ]
    },
 
 ]
 console.log(quiz)
+
+
+
